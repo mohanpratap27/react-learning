18e163c3fef54615caa02034dd751b7d65a93b51
mohanpratap27 <126937993+mohanpratap27@users.noreply.github.com>
2023-03-06 16:37:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import React, { Component } from "react";
 import Person from "./components/person";
-function App() {
-  return (
-    <div className="App">
-      <Person />
-      
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [
+        {
+          name: "demo One",
+        },
+        {
+          name: "dmo Two",
+        },
 
+        {
+          name: "demo three",
+        },
+        {
+            name: "demo four",
+          },
+      ],
+    };
+  }
+
+  render() {
+    let persons;
+    persons = this.state.persons.map((p) => {
+      return <Person name={p.name}/>
+    });
+    return <div className="App">{persons}</div>
+  }
+}
 export default App;
