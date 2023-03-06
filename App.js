ef75ea14a4c7fb4e0c4d2d0b05cac8b17659bab6
mohanpratap27 <126937993+mohanpratap27@users.noreply.github.com>
2023-03-06 11:19:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Person from "./components/person";
+function App() {
+  return (
+    <div className="App">
+      <Person />
+      
+    </div>
+  );
+}
+
+export default App;
